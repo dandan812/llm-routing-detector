4d5c3bca6195edca6f3f835db1da0556b88078d1
蛋蛋侠 <2574355723@qq.com>
2026-08-18 14:39:56 +0800

    Delete test_runtime_compat.py

--- test_runtime_compat.py
+++
@@ -1,5 +0,0 @@
-def test_detector_and_server_import_on_supported_runtime():
-    # Importing these modules catches runtime-evaluated typing constructs and
-    # other compatibility regressions before the desktop launcher is used.
-    import gpt56_vnext.detector  # noqa: F401
-    import gpt56_vnext.server  # noqa: F401
